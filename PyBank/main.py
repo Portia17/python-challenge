17385861c9fdb312699ddbea9b65d7197bb1c1c8
Portia17 <popr2001@colorado.edu>
2023-03-21 17:22:16 -0700

    added length

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,37 @@
 #get it to read CSV file
 import os
 import csv
-csvpath = "../Resources/budget_data.csv"
+csvpath = "Resources/budget_data.csv"
 
+#print
+print("Financial Analysis")
+print("-------------------------------------------")
+
+# open csv
+with open(csvpath) as csvfile:
+    data = csv.reader(csvfile, delimiter=",")
+
+    csvheader = next(data)
+    #print(f"CSV Header: {csvheader}")
+
+    rowcount = 0
+    for row in data:
+        rowcount= rowcount + 1
+    print("Total Months: " + str(rowcount))
+    
+
+
+       
+
+     
+
+
+
+
+#         #get number of months
+#         months = len()
+#         print ("Total Months: " + months)
+
+
+
+# find total number of months
---
+++ PyPoll/main.py
@@ -0,0 +1,4 @@
+#get it to read csv
+import os
+import csv
+csvpath = "Resources/election_data.csv"
